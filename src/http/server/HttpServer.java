1457a66a6cdfb5216acc97f7b48500d8f63059cf
Andrey-SPb1 <honor8x98@gmail.com>
2023-05-14 15:37:05 +0300

    MultithreadingServer/Create WAR

--- src/http/server/HttpServer.java
+++ src/http/server/HttpServer.java
@@ -7,31 +7,40 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
 
+    private final ExecutorService pool;
     private final int port;
+    private boolean stopped;
 
-    public HttpServer(int port) {
+
+    public HttpServer(int port, int poolSize) {
         this.port = port;
+        this.pool = Executors.newFixedThreadPool(poolSize);
     }
 
     public void run() {
         try (ServerSocket serverSocket = new ServerSocket(port)){
-            Socket socket = serverSocket.accept();
-            processSocket(socket);
+            while(!stopped) {
+                Socket socket = serverSocket.accept();
+                pool.submit(() -> processSocket(socket));
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     private void processSocket(Socket socket) {
-
         try(socket;
             DataInputStream inputStream = new DataInputStream(socket.getInputStream());
             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())){
 //            step 1 handle request
             System.out.println("Request: " + new String(inputStream.readNBytes(400)));
+
+            Thread.sleep(10000);
 
 //            step 2 handle response
             byte[] body = Files.readAllBytes(Path.of("resources", "example.html"));
@@ -43,11 +52,13 @@ public class HttpServer {
             outputStream.write(headers);
             outputStream.write(System.lineSeparator().getBytes());
             outputStream.write(body);
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             // TODO: 10.05.2023 log error message 
             e.printStackTrace();
         }
-
     }
 
+    public void setStopped(boolean stopped) {
+        this.stopped = stopped;
+    }
 }
--- src/http/server/HttpServerRunner.java
+++ src/http/server/HttpServerRunner.java
@@ -3,7 +3,7 @@ package http.server;
 public class HttpServerRunner {
 
     public static void main(String[] args) {
-        HttpServer server = new HttpServer(9000);
+        HttpServer server = new HttpServer(9000, 100);
         server.run();
     }
 }
